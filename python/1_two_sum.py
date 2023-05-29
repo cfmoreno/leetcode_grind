4a37f8050017cd89d51551b5a23e069425ffe246
Carlos Moreno <cfmorenod@gmail.com>
2023-05-28 22:35:36 -0400

    added new scripts in python

---
+++ python/1_two_sum.py
@@ -0,0 +1,25 @@
+"""
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+"""
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashm = {}
+        for i, num in enumerate(nums):
+            if target - num in hashm:
+                return [hashm[target - num], i]
+            hashm[num] = i
